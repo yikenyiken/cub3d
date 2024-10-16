fa2f02bb4cf033fa530c357d0a66805f88e08350
Youssef Iken <yiken@e2r5p8.1337.ma>
2024-10-16 12:29:56 +0100

    added floor and ceiling colors drawing functionality

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: yiken <yiken@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 12:52:35 by yiken             #+#    #+#             */
-/*   Updated: 2024/10/13 17:03:03 by yiken            ###   ########.fr       */
+/*   Updated: 2024/10/16 12:15:51 by yiken            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,7 @@ void	init_player(t_player *player, t_data *data)
 {
 	set_player_xy(player, data->mini_map_width / 2, data->mini_map_height / 2);
 
-	player->angle = M_PI / 2;
+	player->angle = M_PI;
 	player->color = 0xFF0000FF;
 	player->radius = 4;
 	player->move_step = 1;
@@ -234,13 +234,13 @@ double	distance_between_points(double x1, double y1, double x2, double y2)
 	return (sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
 }
 
-void	cast_ray(t_ray *ray, double ray_angle, t_mlx *mlx)
+void	cast_ray(t_ray *ray, t_mlx *mlx)
 {
 	double	horz_distance;
 	double	vert_distance;
 
-	find_horz_intersection(ray, ray_angle, mlx);
-	find_vert_intersection(ray, ray_angle, mlx);
+	find_horz_intersection(ray, ray->angle, mlx);
+	find_vert_intersection(ray, ray->angle, mlx);
 	horz_distance = distance_between_points(mlx->player.x, mlx->player.y, ray->horz_intersect_x, ray->horz_intersect_y);
 	vert_distance = distance_between_points(mlx->player.x, mlx->player.y, ray->vert_intersect_x, ray->vert_intersect_y);
 	ray->distance = horz_distance;
@@ -259,12 +259,17 @@ void	cast_rays(t_mlx *mlx)
 	double	ray_angle;
 	int			i;
 
-	ray_angle = mlx->player.angle - (mlx->data->fov / 2);
+	ray_angle = mlx->player.angle - (mlx->data->fov / 2); // since fov == 60deg, you want half of your fov
+	//to the right of the direction you are looking at (player.angle), and let's consider the angle of the
+	//first ray: player.angle - half of fov
 	i = 0;
 	while (i < mlx->data->num_rays)
 	{
-		cast_ray(mlx->data->rays + i, normalize_angle(ray_angle), mlx);
-		ray_angle += mlx->data->fov / mlx->data->num_rays;
+		(mlx->data->rays + i)->angle = normalize_angle(ray_angle);
+		cast_ray(mlx->data->rays + i, mlx);
+		ray_angle += mlx->data->fov / mlx->data->num_rays; // after fiding the angle of the first ray (ray_angle)
+		//you want an increment by which you will advance to get the angle of the second ray (second_ray_angle),
+		//at the same time you don't want to surpass fov 60deg so that ray_angle + (incerement * num_rays) == fov (60deg)
 		i++;
 	}
 }
@@ -388,10 +393,11 @@ void	draw_wall(t_mlx *mlx, int x, double y, double wall_strip_height)
 	double	i;
 
 	i = y;
-	printf("WaStHi: %f, ScHi: %f\n", wall_strip_height, mlx->data->screen_height);
-	while (i < floor(y + wall_strip_height))
+	if (i < 0)
+		i = 0;
+	while (i < (int)(y + wall_strip_height) && i < mlx->data->screen_height)
 	{
-		mlx_put_pixel(mlx->img.frame, x, i, 0x00FF00FF);
+		mlx_put_pixel(mlx->img.frame, x, i, 0x99555194);
 		i++;
 	}
 }
@@ -401,23 +407,53 @@ void	draw_walls(t_mlx *mlx)
 	int		i;
 	double	wall_strip_height;
 	double	distance_projection_plane;
+	double	perp_distance;
+	t_ray	*ray;
 
-	distance_projection_plane = (mlx->data->screen_width / 2) / round(tan(mlx->data->fov / 2));
-	// printf("screen_width: %f\n", mlx->data->screen_width / 2);
-	// printf("tan(half fov): %f\n", tan(mlx->data->fov / 2));
+	distance_projection_plane = (mlx->data->screen_width / 2) / tan(mlx->data->fov / 2);
 	i = 0;
 	while (i < mlx->data->num_rays)
 	{
-		wall_strip_height = (mlx->data->mini_map_tile_size / (mlx->data->rays + i)->distance) * distance_projection_plane;
-		// printf("%f / %f * %f\n", mlx->data->mini_map_tile_size, mlx->data->rays[i].distance, distance_projection_plane);
-		draw_wall(mlx, i, floor((mlx->data->screen_height / 2) - (wall_strip_height / 2)), wall_strip_height);
+		ray = mlx->data->rays + i;
+		perp_distance = ray->distance * cos(ray->angle - mlx->player.angle);
+		wall_strip_height = (mlx->data->mini_map_tile_size / perp_distance) * distance_projection_plane;
+		draw_wall(mlx, i, (int)(mlx->data->screen_height / 2) - (wall_strip_height / 2), wall_strip_height);
+		i++;
+	}
+}
+
+void	draw_ceiling_floor_color(t_mlx *mlx)
+{
+	uint32_t	*color_buffer;
+	uint32_t 	ceiling_color = 0x65FBF1FF; //cloudy sky color
+	uint32_t 	floor_color = 0x5511FFFF; //grass color
+	uint32_t	i;
+	uint32_t	j;
+
+	i = 0;
+	color_buffer = (uint32_t *)mlx->img.frame->pixels;
+	//draw ceiling pixels
+	while (i < mlx->img.frame->height / 2)
+	{
+		j = 0;
+		while (j < mlx->img.frame->width)
+			*(color_buffer + (i * mlx->img.frame->width) + j++) = ceiling_color;
+		i++;
+	}
+	//draw floor pixels
+	i = mlx->img.frame->height / 2;
+	while (i < mlx->img.frame->height)
+	{
+		j = 0;
+		while (j < mlx->img.frame->width)
+			*(color_buffer + (i * mlx->img.frame->width) + j++) = floor_color;
 		i++;
 	}
 }
 
 void	update_frame(t_mlx *mlx)
 {
-	memset(mlx->img.frame->pixels, 0x00000000, mlx->data->screen_width * mlx->data->screen_height * sizeof(uint32_t));
+	draw_ceiling_floor_color(mlx);
 	draw_walls(mlx);
 	draw_map(mlx);
 	draw_rays(mlx);
@@ -457,35 +493,3 @@ int	main(void)
 	mlx_loop(mlx.ptr);
 	mlx_terminate(mlx.ptr);
 }
-
-//WSH wall_strip_height
-
-// what is the issue with getting a WSH larger than screen_height ?
-	// have we used the wrong types (double instead of float) or even int instead of double somewhere ?
-	// is the problem that tan(fov / 2) has to be >= 1
-	// is the problem that (screen_width / 2) is too large
-	// is the problem that (tile_size / ray.distance) is too large
-
-// why are we getting -81 as the y in draw_wall()
-	// what makes WaStHi too large
-	// the formula to calculate distance_to_proj_plane = (SW / 2) / tan(fov / 2)
-	// distance_to_proj_plane = 1662.768775; although half SW = 960
-
-
-// what might be causing the segfault ?
-	// is it uninitialized variables ? likely not!
-	// is it out of bound incerements ? 
-
-// can we fix the ray drawing white gap thing ?
-
-// what are the steps to be taken to render 3d walls ?
-
-// step1: itterate over the rays
-	// there is a ray for each horizontal pixel of the frame
-// step2: for each ray calculate the wall strip height
-	// calculate the distance to the projection plane
-		// distance to projection plane is: (screen_width / 2) / tan(fov / 2)
-	// the wall strip height is: tile_size / ray.distance * distance to projection plane
-// step3: draw strip on frame
-	// the strip is a row of pixels in an x coordinate, starting from: y = (screen_height / 2)
-	// - (WaStHi / 2), and ending at y += WaStHi
